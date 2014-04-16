0912898137c9c088ad9bef4689da5f2a9789edcd
mark-jacobs <your_email@example.com>
2014-04-16 23:14:40 +0100

    another test

--- test.rb
+++ test.rb
@@ -1,3 +1,5 @@
+
+
 # read integer command line integers and multiply all together.
 result = 1
 ARGV.length.times do |each_one|
@@ -7,3 +9,14 @@ end
 # another test
 # display result to screen
 puts 'The result is ' + result.to_s
+
+my_array = []
+result.times do |array_count|
+    # assigns and prints the values
+    puts my_array[array_count] = rand(1000)
+end
+
+# print out the list again.
+puts "Reprint the list:"
+result.times { |next_count| puts my_array[next_count] }
+
